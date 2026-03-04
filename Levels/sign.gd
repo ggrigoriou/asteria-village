700e520d5ca501aac896dc9bec26920c3ca97161
Patrikios Antonis <patrikios@codeup.software>
2026-03-04 18:52:42 +0200

    fix the sign

--- Levels/sign.gd
+++ Levels/sign.gd
@@ -1,19 +1,77 @@
-extends StaticBody2D
+extends Node2D
 
-@onready var interactable: Area2D = $Interactable
-var is_showing := false
+@onready var interactable: Interactable = $Interactable
+var info_timer: Timer
 
-@export var sign_text: String = "Welcome to the village."
+var infos: Array[String] = [
+	"At the top-right of the map,\nyou’ll find the Farmer.",
+	"At the bottom-left of the map,\nyou’ll find the Miller.",
+	"At the top-center of the map,\nyou’ll find the Mage and the Mystic Well.",
+	"Good luck exploring the village!"
+]
+
+var info_index := 0
+var cycling := false
+var seconds_left := 5
+
+const DEFAULT_TEXT := "Welcome to the village. Directions"
+const COUNTDOWN_START := 5
 
 func _ready() -> void:
-	interactable.interact = _on_interact
+	interactable.interact_name = DEFAULT_TEXT
+	interactable.interact = Callable(self, "_instractions")
 
-func _on_interact() -> void:
-	var dialogue = get_tree().current_scene.get_node_or_null("DialogueUI")
-	if not dialogue:
+	info_timer = Timer.new()
+	info_timer.wait_time = 1.0
+	info_timer.one_shot = false
+	add_child(info_timer)
+	info_timer.timeout.connect(_on_info_timer_timeout)
+
+func _instractions() -> void:
+	if not cycling:
+		# First press: start from the beginning
+		cycling = true
+		info_index = 0
+		seconds_left = COUNTDOWN_START
+		_update_interact_text()
+		info_timer.start()
 		return
 
-	dialogue.show_dialogue(sign_text)
+	# Pressed again while cycling: go to next info immediately
+	info_index += 1
 
-	while dialogue.is_open():
-		await get_tree().process_frame
+	if info_index >= infos.size():
+		# If it was the last one, finish and reset
+		_finish_cycle()
+		return
+
+	seconds_left = COUNTDOWN_START
+	_update_interact_text()
+	info_timer.start() # restart so it feels consistent
+
+func _on_info_timer_timeout() -> void:
+	if not cycling:
+		return
+
+	seconds_left -= 1
+
+	if seconds_left <= 0:
+		info_index += 1
+
+		if info_index >= infos.size():
+			_finish_cycle()
+			return
+
+		seconds_left = COUNTDOWN_START
+
+	_update_interact_text()
+
+func _update_interact_text() -> void:
+	interactable.interact_name = "%s (%d)" % [infos[info_index], seconds_left]
+
+func _finish_cycle() -> void:
+	cycling = false
+	info_timer.stop()
+	info_index = 0
+	seconds_left = COUNTDOWN_START
+	interactable.interact_name = DEFAULT_TEXT
